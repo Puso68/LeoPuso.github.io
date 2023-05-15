13943484bd623e46810335b9a2f4b8da3e89d342
puso68@msn.com <Puso68>
2023-05-15 15:16:54 -0700

    A6

--- A6/starter/encrypt-it.js
+++ A6/starter/encrypt-it.js
@@ -10,6 +10,7 @@
    * on the page. When this event occurs, the attached function (init) will be called.
    */
   window.addEventListener("load", init);
+  
 
   /**
    * TODO: Write a function comment using JSDoc.
@@ -24,3 +25,71 @@
   // you shouldn't write an entire Java program in the main method).
 
 })();
+
+function handleClick() {
+  var textarea = document.getElementById("input-text");
+  var cipherResult = shiftCipher(textarea.value);
+
+  var resultElement = document.getElementById("result");
+  resultElement.textContent = cipherResult;
+}
+
+
+/**
+ * Returns an encrypted version of the given text, where
+ * each letter is shifted alphabetically ahead by 1 letter,
+ * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+ */
+function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { // letter is between 'a' and 'y'
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+
+  return result;
+}
+
+function textSize12pt() {
+  var textarea = document.getElementById("input-text");
+  var resultElement = document.getElementById("result");
+
+  textarea.style.fontSize = "12pt"
+  resultElement.style.fontSize = "12pt"
+
+}
+
+function textSize24pt() {
+  var textarea = document.getElementById("input-text");
+  var resultElement = document.getElementById("result");
+
+  textarea.style.fontSize = "24pt"
+  resultElement.style.fontSize = "24pt"
+
+}
+
+function allCaps() {
+  var textarea = document.getElementById("input-text");
+  var resultElement = document.getElementById("result");
+ 
+
+  var checkbox = document.getElementById("all-caps");
+  
+  if (checkbox.checked) {
+    textarea.value = textarea.value.toUpperCase();
+    resultElement.textContent = resultElement.textContent.toUpperCase();
+    
+  } else {
+    textarea.value = textarea.value.toLowerCase();
+    resultElement.textContent = resultElement.textContent.toLowerCase();
+  }
+
+}
