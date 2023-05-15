284f8500535f4c44ef7aca76a297b176286f6c0a
puso68@msn.com <Puso68>
2023-05-15 13:49:51 -0700

    reset/clear buttons

--- A5/fancifymytext.js
+++ A5/fancifymytext.js
@@ -1,3 +1,5 @@
+
+
 function biggerText() {
     
     var textarea = document.getElementById("mineText");
@@ -22,4 +24,27 @@ function moo() {
     var parts = textarea.value.split(" ");
     textarea.value = parts.join("-MOO ");
   }
+
+function resetStyling() {
+    var textarea = document.getElementById("mineText");
+    textarea.style.textDecoration = "none"; // Reset text decoration
+    textarea.style.color = "black";
+    textarea.style.fontWeight="normal";
+    textarea.style.fontSize = "initial"; // Reset font size
+    textarea.value = textarea.value.replace(/-MOO /g, " "); // Remove "-MOO " from the text
+    var text = textarea.value.toLowerCase(); // Convert the entire text to lowercase
+    var formattedText = text.charAt(0).toUpperCase() + text.slice(1); // Capitalize the first letter
+    textarea.value = formattedText; // Update the textarea with the formatted text
+
+}
+  
+  
+  
+function resetAll() {
+    var textarea = document.getElementById("mineText");
+    textarea.value = ""; // Clear the textarea
+    textarea.style.textDecoration = "none"; // Reset text decoration
+    textarea.style.fontSize = "initial"; // Reset font size
+
+}
   
